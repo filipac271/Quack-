6c080b206de06945d4c33a0866aa3637ca5ac5ed
andreia <a106915@alunos.uminho.pt>
2024-03-13 13:51:51 +0000

    Feat:homepage

--- quack/app/page.tsx
+++ quack/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import patotestes from "@/Images/imgPato.png"
 
 export default function Home() {
   return (
-    <main>
-      <h1>Quack</h1>
+    <main className="bg-yellow-50">
+      <Image src= {patotestes} alt="imagem"></Image>
+      <h1 className ="text-yellow-500 text-opacity-100 text-center text-5xl font-sans font-semibold">Store</h1>
       <Link href="/about">About</Link>
       <Link href="/carrinho"> Carrinho</Link>
     </main>
