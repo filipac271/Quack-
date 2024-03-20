e5de52c4c99b08c53e6e3b77e943f8eaab96239c
a107329 <a107329@alunos.uminho.pt>
2024-03-20 10:25:31 +0000

    Footer finalizado com as redes sociais, falta depois alterar as cores para ficar a combinar com o resto

--- quack/app/components/Footer.tsx
+++ quack/app/components/Footer.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
+import {FaFacebookF, FaInstagram, FaTwitterSquare} from "react-icons/fa";
+import {FaFacebook} from "react-icons/fa";
+import {FaTwitter} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
+
+
+
+
+
 const Footer = () => {
   return (
-    <footer className=' max-w-7xl mx-auto  justify-between flex-col min-h-screen mt-auto flex  insert-x-0 bottom-0'>
+    <footer className=' max-w-7xl mx-auto  justify-between flex-col min-h-screen mt-auto flex insert-x-0 bottom-0'>
 			<div className=' bg-amber-100 flex flex-row mt-auto'>
       <div className='flex-1 flex flex-end justify-between mt-auto'>
       <div className=' '>
-        <ul className='p-5'>
-          <p className='text-gray-800 font-bold text-3xl pb-6'>Sobre Nós </p>
-          <p className='text-gray-600 font-semibold hover:text-gray-900'>A tua empresa patástica e quack quack quack</p>
+        <ul className='p-4'>
+          <p className='text-gray-800 font-bold text-2xl pb-4'>Sobre Nós </p>
+          <p className='text-gray-600 text-base font-semibold hover:text-gray-900'>A tua empresa patástica e quack quack quack</p>
         </ul>
       </div>
       <div className="">
-					<ul className='p-5'>
+					<ul className='p-4'>
 						<p className="text-gray-800 font-bold text-2xl pb-4">Coleções</p>
 						<li className="text-gray-600 text-md pb-2 font-semibold hover:text-gray-900 cursor-pointer">
 							Nova coleçao
@@ -20,16 +29,11 @@ const Footer = () => {
 						<li className="text-gray-600 text-md pb-2 font-semibold  hover:text-gray-900 cursor-pointer">
             Nova coleçao
 						</li>
-						<li className="text-gray-600 text-md pb-2 font-semibold hover:text-gray-900 cursor-pointer">
-            Nova coleçao
-						</li>
-						<li className="text-gray-600 text-md pb-2 font-semibold  hover:text-gray-900 cursor-pointer">
-            Nova coleçao
-						</li>
+						
 					</ul>
 				</div>
         <div className="">
-					<ul className='p-5'>
+					<ul className='p-4'>
 						<p className="text-gray-800 font-bold text-2xl pb-4">Contactos</p>
 						<li className="text-gray-600 text-md pb-2 font-semibold hover:text-gray-900 cursor-pointer">
 							Email : quack@quack.pt
@@ -40,19 +44,22 @@ const Footer = () => {
 					</ul>
 				</div>
 				<div className="">
-					<ul className='p-5'>
-						<p className="text-gray-800 font-bold text-2xl pb-4">cena</p>
-						<li className="text-gray-600 text-md pb-2 font-semibold hover:text-gray-900 cursor-pointer">
-							quack quack quack
-						</li>
-						<li className="text-gray-600 text-md pb-2 font-semibold  hover:text-gray-900 cursor-pointer">
-            quack quack quack
-						</li>
+					<ul className='p-4'>
+						<p className="text-gray-800 font-bold text-2xl pb-4">Redes Sociais</p>
+						<ul className='flex flex-wrap gap-x-4'>
+							<FaInstagram className='text-xl cursor-pointer hover:text-yellow-400'/>
+							<FaFacebookF className='text-xl cursor-pointer hover:text-yellow-400'/>
+							<FaLinkedin className='text-xl cursor-pointer hover:text-yellow-400'/>
+							<FaTwitterSquare className='text-xl cursor-pointer hover:text-yellow-400'/>
+
+
+						</ul>
 					</ul>
 				</div>
 				</div>
         </div>
-        <div className="flex flex-row justify-center items-center text-center w-full p-5 bg-gray-50 align-content:flex-end;">
+
+        <div className="flex flex-row justify-center items-center text-center w-full p-3 bg-gray-50 align-content:flex-end;">
 				<h1 className=" text-gray-800 font-semibold">
 					© 2023-2024 All rights reserved | Build with ❤ by{" "}
 					<span className="hover:text-blue-600 font-semibold cursor-pointer">
