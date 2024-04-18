e7a7cab4189693fdd09ff4d40981e11a607fd227
andreia <a106915@alunos.uminho.pt>
2024-04-18 17:11:58 +0100

    Frontend da home

--- quack/app/page.tsx
+++ quack/app/page.tsx
@@ -84,188 +84,188 @@ export default function Home() {
         <h1 className="mt-20 ml-10 text-3xl underline font-serif ">Patos clássicos:</h1>
 
         <div className="mt-10 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center" >
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoAmarelo} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Amarelo</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Amarelo</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoLaranja} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Laranja</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Laranja</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoRosa} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Rosa</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Rosa</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoVermelho} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Vermelho</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Vermelho</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
 
         <div className="mt-20 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center">
-        <div className="hover:shadow-2xl">
+        <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoPreto} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Preto</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Preto</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoAzul} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Azul</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Azul</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoAzulClaro} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Azul Claro</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Azul Claro</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoVerde} alt="imagem"/>
-            <h1 className="text-center">Pato Clássico Verde</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Clássico Verde</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
 
         <div className="mt-20 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center" >
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCoroaAmarelo} alt="imagem"/>
-            <h1 className="text-center">Pato Coroa Amarelo</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Coroa Amarelo</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCoroaRosa} alt="imagem"/>
-            <h1 className="text-center">Pato Coroa Rosa</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Coroa Rosa</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCoroaAzul} alt="imagem"/>
-            <h1 className="text-center">Pato Coroa Azul</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Coroa Azul</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCoroaVermelho} alt="imagem"/>
-            <h1 className="text-center">Pato Coroa Vermelho</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Coroa Vermelho</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
 
         <h1 className="mt-20 ml-10 text-3xl underline font-serif ">Animais:</h1>
 
         <div className="mt-20 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center" >
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoAbelha} alt="imagem"/>
-            <h1 className="text-center">Pato Abelha</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Abelha</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoBurro} alt="imagem"/>
-            <h1 className="text-center">Pato Burro</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Burro</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCamelo} alt="imagem"/>
-            <h1 className="text-center">Pato Camelo</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Camelo</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCanguru} alt="imagem"/>
-            <h1 className="text-center">Pato Canguru</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Canguru</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
 
         <div className="mt-20 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center " >
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCaracol} alt="imagem"/>
-            <h1 className="text-center">Pato Caracol</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Caracol</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoCavalo} alt="imagem"/>
-            <h1 className="text-center">Pato Cavalo</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Cavalo</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoTubarao} alt="imagem"/>
-            <h1 className="text-center">Pato Tubarão</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Tubarão</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoElefante} alt="imagem"/>
-            <h1 className="text-center">Pato Elefante</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Elefante</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
 
         <div className="mt-20 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center" >
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoFlamingo} alt="imagem"/>
-            <h1 className="text-center">Pato Flamingo</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Flamingo</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoVaca} alt="imagem"/>
-            <h1 className="text-center">Pato Vaca</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Vaca</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoTouro} alt="imagem"/>
-            <h1 className="text-center">Pato Touro</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Touro</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoJoaninha} alt="imagem"/>
-            <h1 className="text-center">Pato Joaninha</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Joaninha</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
 
         <div className="mt-20 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center" >
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoLeao} alt="imagem"/>
-            <h1 className="text-center">Pato Leão</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Leão</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoMacaco} alt="imagem"/>
-            <h1 className="text-center">Pato Macaco</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Macaco</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoMorcego} alt="imagem"/>
-            <h1 className="text-center">Pato Morcego</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Morcego</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoOvelha} alt="imagem"/>
-            <h1 className="text-center">Pato Ovelha</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Ovelha</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
 
         <div className="mt-20 mb-10 flex flex-row flex-nowrap gap-x-32 justify-center" >
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoPavao} alt="imagem"/>
-            <h1 className="text-center">Pato Pavão</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Pavão</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoPinguim} alt="imagem"/>
-            <h1 className="text-center">Pato Pinguim</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Pinguim</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoPorco} alt="imagem"/>
-            <h1 className="text-center">Pato Porco</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Porco</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div>
-          <div className="hover:shadow-2xl">
+          <div className="hover:shadow-2xl hover:underline hover:font-semibold">
             <Image src={PatoTartaruga} alt="imagem"/>
-            <h1 className="text-center">Pato Tartaruga</h1>
-            <h1 className="text-center">2499.99€</h1>
+            <h1 className="text-center font-mono text-xl">Pato Tartaruga</h1>
+            <h1 className="text-center font-mono text-xl">5000€</h1>
           </div> 
         </div>
         
