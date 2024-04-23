2c62d0f09349220c3bec35cb3afccef5af9a567d
andreiacardoso13 <147544311+andreiacardoso13@users.noreply.github.com>
2024-04-23 15:10:19 +0100

    Feat:homepage (#1)
    * Feat:homepage
    
    * HomePage
    
    * Frontend da home
    
    * Melhoria de código
    
    * Adição de botão de carrinho e melhoria do código
    
    * Fronted Carrinho
    
    * Melhoria da lista de produtos
    
    ---------
    
    Co-authored-by: andreia <a106915@alunos.uminho.pt>

--- quack/app/carrinho/page.tsx
+++ quack/app/carrinho/page.tsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import Image from "next/image";
+import { carrinhoL} from "@/app/components/ListaProdutos";
 
 const Carrinho = () => {
   return (
-    <div>Carrinho</div>
+    <main>
+      <div className="flex justify-center ml-10 mt-10 font-serif text-6xl">Carrinho</div>
+      <div className="bg-yellow-100 ml-20 mt-20 mr-20 rounded-xl border-4 border-yellow-300">
+        <div className = "grid grid-cols-2 gap-4 ">
+          <h1 className = "underline mt-10 flex justify-center font-mono text-4xl ">Produto</h1>
+          <h1 className = "mr-10 underline mt-10 flex justify-center font-mono text-4xl">Preço</h1>
+        </div>
+          <div>
+          {carrinhoL.map((produto) => (
+             <div key={produto.nome} className="bg-white hover:bg-gray-100 hover:shadow-2xl rounded border-2 ml-10 mt-10 mb-10 mr-10 hover:underline hover:font-semibold grid grid-cols-2 gap-4">
+               <div className="grid grid-cols-2 gap-4">
+                <Image className="size-48" src={produto.imagem} alt={produto.nome} />
+                <h1 className="text-center flex items-center font-mono text-3xl">{produto.nome}</h1>
+               </div>
+               <h1 className="flex items-center justify-center font-mono text-3xl">{produto.preco}</h1>
+             </div>
+           ))}
+        </div>  
+        </div>
+        <div className="bg-yellow-100 grid grid-cols-2 gap-4 ml-56 mr-56 mt-10 mb-10 rounded-xl border-4 border-yellow-300">
+          <div className="ml-10">
+            <h1 className="mt-10 text-mono underline font-medium text-3xl">Método de envio:</h1>
+            <div className="cursor-pointer hover:bg-yellow-200 hover:bg-opacity-50 mt-10 border-2 border-yellow-100 hover:font-medium hover:rounded-lg hover:border-2 hover:border-yellow-600 text-mono text-2xl"> Método de envio padrão : Free</div>
+            <div className="cursor-pointer hover:bg-yellow-200 hover:bg-opacity-50 mb-10 border-2 border-yellow-100 hover:font-medium hover:rounded-lg hover:border-2 hover:border-yellow-600 text-mono text-2xl">Envio Expresso : 3,90€</div>
+          </div>
+          <div>
+           <h1 className="mt-10 text-mono underline font-medium text-3xl">Pagamento:</h1>
+           <div className = "mt-10 text-mono text-2xl grid grid-cols-2 gap-4 mr-10">
+            <div>
+               <div>SubTotal:</div>
+               <div>Custo Envio:</div>
+             </div>
+             <div>
+                <div className = "flex justify-end">90€</div>
+                <div className = "flex justify-end">0€</div>
+              </div>
+           </div>
+           <div className = "border-t-2 border-black text-mono text-2xl grid grid-cols-2 gap-4 mr-10">
+            <div>
+               <div className = "font-medium">Total:</div>
+             </div>
+             <div>
+                <div className = "font-medium flex justify-end">90€</div>
+              </div>
+           </div>
+           <div className="cursor-pointer hover:shadow-2xl hover:shadow-gray-600 mt-10 mb-10 mr-10 rounded-xl bg-yellow-600 font-semibold text-2xl font-mono text-white flex justify-center ">Checkout</div>
+          </div>
+        </div>
+    </main>
   )
 }
 
--- quack/app/page.tsx
+++ quack/app/page.tsx
@@ -1,12 +1,82 @@
 import Image from "next/image";
-import Link from "next/link";
+import patoLGBT from "@/Images/PatoLGBT.png"
+import { carrinhoL, tendencias, classicos, animais, profissoes, herois} from '@/app/components/ListaProdutos';
 
-export default function Home() {
+
+
+
+
+ export default function Home() {
   return (
-    <main>
-      <h1>Quack</h1>
-      <Link href="/about">About</Link>
-      <Link href="/carrinho"> Carrinho</Link>
-    </main>
+      <main>
+        <div className="bg-gradient-to-r from-yellow-500 to-yellow-200 flex flex-row gap-x-60">
+          <Image className="transform size-100" src={patoLGBT} alt="imagem"></Image>
+          <h1 className="text-black text-opacity-100 text-5xl mt-52 font-serif font-thin underline decoration-black decoration-1 underline-offset-8">Resolvemos todos os teus code-problems Quack</h1>
+        </div>
+        <div>
+         <h1 className="mt-20 text-left ml-10 text-5xl font-serif font-thin margin"> Tendências: </h1>
+         <div className="mt-10 mb-10 grid grid-cols-4 gap-4 justify-center">
+           {tendencias.map((produto) => (
+             <div key={produto.nome} className="hover:underline hover:font-semibold">
+               <Image className="hover:shadow-2xl ml-16" src={produto.imagem} alt={produto.nome} />
+               <h1 className="text-center font-mono text-xl">{produto.nome}</h1>
+               <h1 className="text-center font-mono text-xl">{produto.preco}</h1>
+             </div>
+           ))}
+         </div>
+       </div>
+       <div>
+        <h1 className="mt-20 text-left ml-10 text-5xl font-serif font-thin margin">Patos Clássicos:</h1>
+        <div className="mt-10 mb-10 grid grid-cols-4 gap-4 justify-center">
+          {classicos.map((produto) => (
+             <div key={produto.nome} className="hover:font-semibold flex flex-col items-center group">
+               <Image className="hover:shadow-2xl" src={produto.imagem} alt={produto.nome} />
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.nome}</h1>
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.preco}</h1>
+               <button className="bg-gradient-to-r from-yellow-600 to-yellow-500 text-white px-4 py-2 rounded invisible group-hover:visible">Adicionar ao carrinho</button>
+             </div>
+            ))}
+           </div>
+       </div>
+       <div>
+        <h1 className="mt-20 text-left ml-10 text-5xl font-serif font-thin margin"> Animais </h1>
+        <div className="mt-10 mb-10 grid grid-cols-4 gap-4 justify-center">
+          {animais.map((produto) => (
+             <div key={produto.nome} className="hover:font-semibold flex flex-col items-center group">
+               <Image className="hover:shadow-2xl" src={produto.imagem} alt={produto.nome} />
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.nome}</h1>
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.preco}</h1>
+               <button className="bg-gradient-to-r from-yellow-600 to-yellow-500 text-white px-4 py-2 rounded invisible group-hover:visible">Adicionar ao carrinho</button>
+             </div>
+           ))}
+         </div>
+       </div>
+       <div>
+        <h1 className="mt-20 text-left ml-10 text-5xl font-serif font-thin margin"> Patos Trabalhadores </h1>
+        <div className="mt-10 mb-10 grid grid-cols-4 gap-4 justify-center">
+          {profissoes.map((produto) => (
+             <div key={produto.nome} className="hover:font-semibold flex flex-col items-center group">
+               <Image className="hover:shadow-2xl" src={produto.imagem} alt={produto.nome} />
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.nome}</h1>
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.preco}</h1>
+               <button className="bg-gradient-to-r from-yellow-600 to-yellow-500 text-white px-4 py-2 rounded invisible group-hover:visible">Adicionar ao carrinho</button>
+             </div>
+           ))}
+         </div>
+       </div>
+       <div>
+        <h1 className="mt-20 text-left ml-10 text-5xl font-serif font-thin margin"> Super-Patos </h1>
+        <div className="mt-10 mb-10 grid grid-cols-4 gap-4 justify-center">
+          {herois.map((produto) => (
+             <div key={produto.nome} className="hover:font-semibold flex flex-col items-center group">
+               <Image className="hover:shadow-2xl" src={produto.imagem} alt={produto.nome} />
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.nome}</h1>
+               <h1 className="group-hover:underline text-center font-mono text-xl">{produto.preco}</h1>
+               <button className="bg-gradient-to-r from-yellow-600 to-yellow-500 text-white px-4 py-2 rounded invisible group-hover:visible">Adicionar ao carrinho</button>
+             </div>
+           ))}
+         </div>
+       </div>
+     </main>
   );
-}
+ }
